c9b42e033d438c026adf638d34c0a1018188ad4c
Codingwal <florian.herzog08@gmail.com>
2024-06-29 12:32:16 +0200

    Cleanup (Bookmarks)

--- Assets/5_Terrain/Scripts/MapGenerator.cs
+++ Assets/5_Terrain/Scripts/MapGenerator.cs
@@ -9,8 +9,6 @@ public class MapGenerator : Singleton<MapGenerator>
     [HideInInspector] public TerrainSettings terrainSettings;
 
     public Material terrainMaterial;
-    readonly int[] possibleMapChunkSizes = { 121, 241 };
-    // [Dropdown("possibleMapChunkSizes")]
     public int chunkSize = 241;
     public int chunksPerSector1D = 9;
     public int vertexIncrement = 1;
@@ -58,8 +56,7 @@ public class MapGenerator : Singleton<MapGenerator>
     }
     void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback)
     {
-        MeshData meshData = MeshGenerator.GenerateTerrainMesh
-        (mapData.map, lod, vertexIncrement);
+        MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.map, lod);
         lock (meshDataThreadInfoQueue)
         {
             meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
