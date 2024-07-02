132a342ecb5140367e0b9cfca0009758e13d6ea9
Codingwal <florian.herzog08@gmail.com>
2024-07-02 16:09:51 +0200

    Improved documentation, small improvements

--- Assets/4_Player/Movement/PlayerLook.cs
+++ Assets/4_Player/Movement/PlayerLook.cs
@@ -16,14 +16,11 @@ public class PlayerLook : MonoBehaviour
     }
     public void ProcessLook(Vector2 input)
     {
-        float mouseX = input.x;
-        float mouseY = input.y;
-
-        xRotation -= mouseY * Time.deltaTime * ySensitivity;
+        // Apply up/down rotation only to the camera, rotation around the x axis to the player which will also rotate the camera
+        xRotation -= input.y * Time.deltaTime * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-
-        transform.Rotate(Vector3.up * mouseX * Time.deltaTime * xSensitivity);
+        transform.Rotate(input.x * Time.deltaTime * xSensitivity * Vector3.up);
     }
 }
