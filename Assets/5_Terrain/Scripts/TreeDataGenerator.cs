2cb0ff78b283e13c4ee6e23990d3320f4c24f9b1
Codingwal <florian.herzog08@gmail.com>
2024-07-03 17:19:41 +0200

    Trees: Improved generation
    The way a minimum distance between trees is garantueed changed. The new variant has the following advantages:
    - Less visible grid
    - Faster
    - Less visible chunk borders

--- Assets/5_Terrain/Scripts/TreeDataGenerator.cs
+++ Assets/5_Terrain/Scripts/TreeDataGenerator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public static class TreeDataGenerator
@@ -11,48 +13,74 @@ public static class TreeDataGenerator
         const float propabilityLerpMinHeightEnd = 30;
         const float propabilityLerpMinSlope = 0.4f;
         const float propabilityLerpMaxSlope = 0.8f;
-        const float maxPositioningOffset = 3;
+        const float maxPositioningOffset = 0.5f;
+        const int minTreeDistance = 2;
+        const float minProbability = 0f;
 
-        float[,] propabilityMap = new float[chunkSize, chunkSize];
-        for (int x = 0; x < chunkSize; x += 7)
+        List<Vector2Int> pointsToGenerate = GetPointsToGenerate(seed, chunkCenter, chunkSize, minTreeDistance);
+
+        foreach (Vector2Int point in pointsToGenerate)
         {
-            for (int y = 0; y < chunkSize; y += 7)
-            {
-                Vector2 pos = new Vector2(x, y) + chunkCenter - new Vector2(chunkSize / 2, chunkSize / 2);
-                float probability = Mathf.PerlinNoise((seed + pos.x) / perlinNoiseScale, (seed + pos.y) / perlinNoiseScale);
+            Vector2 pos = point + chunkCenter - new Vector2(chunkSize / 2, chunkSize / 2);
+            float probability = Mathf.PerlinNoise((seed + pos.x) / perlinNoiseScale, (seed + pos.y) / perlinNoiseScale);
 
-                // Limit the probability to 1 so that exceeding the maxHeight or maxSlope garantuees that a tree isn't generated
-                probability = Mathf.Min(probability, 1);
-                // Debug.LogWarning(probability);
+            // Limit the probability to 1 so that exceeding the maxHeight or maxSlope garantuees that a tree isn't generated
+            probability = Mathf.Min(probability, 1);
+            // Debug.LogWarning(probability);
 
-                // Reduce the propability for steep and/or high vertices
-                probability -= Mathf.InverseLerp(propabilityLerpMaxHeightBegin, propabilityLerpMaxHeightEnd, map.map[x, y].height);
-                probability -= Mathf.InverseLerp(propabilityLerpMinHeightEnd, propabilityLerpMinHeightBegin, map.map[x, y].height);
-                probability -= Mathf.InverseLerp(propabilityLerpMinSlope, propabilityLerpMaxSlope, Slope(map.map, x, y));
+            // Reduce the propability for steep and/or high vertices
+            probability -= Mathf.InverseLerp(propabilityLerpMaxHeightBegin, propabilityLerpMaxHeightEnd, map.map[point.x, point.y].height);
+            probability -= Mathf.InverseLerp(propabilityLerpMinHeightEnd, propabilityLerpMinHeightBegin, map.map[point.x, point.y].height);
+            probability -= Mathf.InverseLerp(propabilityLerpMinSlope, propabilityLerpMaxSlope, Slope(map.map, point.x, point.y));
 
-                propabilityMap[x, y] = probability;
-            }
+            if (!(probability > minProbability)) continue;
+
+            System.Random rnd = new(seed + 100 + point.x * point.y ^ 2);
+            Vector2 localPos = point;
+            localPos += 2 * maxPositioningOffset * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
+            map.trees.Add(new(localPos, 1));
         }
 
-        for (int x = 0; x < chunkSize; x++)
-        {
-            for (int y = 0; y < chunkSize; y++)
-            {
-                // Generate a deterministic, position and seed dependent random value between 0 and 1
-                System.Random rnd = new(seed + 879 * x * y ^ 2);
-                float value = (float)rnd.NextDouble();
+        // float[,] propabilityMap = new float[chunkSize, chunkSize];
+        // for (int x = 0; x < chunkSize; x += 7)
+        // {
+        //     for (int y = 0; y < chunkSize; y += 7)
+        //     {
+        //         Vector2 pos = new Vector2(x, y) + chunkCenter - new Vector2(chunkSize / 2, chunkSize / 2);
+        //         float probability = Mathf.PerlinNoise((seed + pos.x) / perlinNoiseScale, (seed + pos.y) / perlinNoiseScale);
 
-                // The greater the propability, the likelier it is for a tree to be generated
-                if (!(value < propabilityMap[x, y])) continue;
+        //         // Limit the probability to 1 so that exceeding the maxHeight or maxSlope garantuees that a tree isn't generated
+        //         probability = Mathf.Min(probability, 1);
+        //         // Debug.LogWarning(probability);
 
-                Vector2 pos = new(x, y);
-                pos += 2 * maxPositioningOffset * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
+        //         // Reduce the propability for steep and/or high vertices
+        //         probability -= Mathf.InverseLerp(propabilityLerpMaxHeightBegin, propabilityLerpMaxHeightEnd, map.map[x, y].height);
+        //         probability -= Mathf.InverseLerp(propabilityLerpMinHeightEnd, propabilityLerpMinHeightBegin, map.map[x, y].height);
+        //         probability -= Mathf.InverseLerp(propabilityLerpMinSlope, propabilityLerpMaxSlope, Slope(map.map, x, y));
 
-                pos = new Vector2(Mathf.Clamp(pos.x, 0, chunkSize - 1), Mathf.Clamp(pos.y, 0, chunkSize - 1));
+        //         propabilityMap[x, y] = probability;
+        //     }
+        // }
 
-                map.trees.Add(new(pos, 1));
-            }
-        }
+        // for (int x = 0; x < chunkSize; x++)
+        // {
+        //     for (int y = 0; y < chunkSize; y++)
+        //     {
+        //         // Generate a deterministic, position and seed dependent random value between 0 and 1
+        //         System.Random rnd = new(seed + 879 * x * y ^ 2);
+        //         float value = (float)rnd.NextDouble();
+
+        //         // The greater the propability, the likelier it is for a tree to be generated
+        //         if (!(value < propabilityMap[x, y])) continue;
+
+        //         Vector2 pos = new(x, y);
+        //         pos += 2 * maxPositioningOffset * new Vector2((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
+
+        //         pos = new Vector2(Mathf.Clamp(pos.x, 0, chunkSize - 1), Mathf.Clamp(pos.y, 0, chunkSize - 1));
+
+        //         map.trees.Add(new(pos, 1));
+        //     }
+        // }
     }
     static float Slope(VertexData[,] map, int x, int y)
     {
@@ -61,5 +89,60 @@ public static class TreeDataGenerator
         y = Mathf.Max(1, y);
 
         return Mathf.Abs(map[x, y].height - map[x - 1, y - 1].height);
+
+    }
+    static List<Vector2Int> GetPointsToGenerate(int seed, Vector2Int chunkCenter, int chunkSize, int minTreeDistance)
+    {
+        List<Vector2Int> pointsToGenerate = new();
+
+        float[,] randomValues = new float[chunkSize, chunkSize];
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                randomValues[x, y] = GetRandomValue(seed, x + chunkCenter.x, y + chunkCenter.y);
+            }
+        }
+
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                if (ShouldGenerateTree(randomValues, x, y, seed, minTreeDistance))
+                    pointsToGenerate.Add(new(x, y));
+            }
+        }
+        return pointsToGenerate;
+    }
+    static bool ShouldGenerateTree(float[,] randomValues, int x, int y, int seed, int minTreeDistance)
+    {
+        float value = GetRandomValue(seed, x, y);
+        for (int offsetX = -minTreeDistance; offsetX <= minTreeDistance; offsetX++)
+        {
+            for (int offsetY = -minTreeDistance; offsetY <= minTreeDistance; offsetY++)
+            {
+                int px = x + offsetX;
+                int py = y + offsetY;
+
+                float neighbourValue;
+                if (px > 0 && py > 0 && px < randomValues.GetLength(0) && py < randomValues.GetLength(1))
+                {
+                    neighbourValue = randomValues[px, py];
+                }
+                else
+                {
+                    neighbourValue = GetRandomValue(seed, px, py);
+                }
+                if (neighbourValue > value)
+                    return false;
+            }
+        }
+        return true;
+    }
+    static float GetRandomValue(int seed, int x, int y)
+    {
+        // Generate a deterministic, position and seed dependent random value between 0 and 1
+        System.Random rnd = new(seed + x * y ^ 2);
+        return (float)rnd.NextDouble();
     }
 }
