9f89a3a994b650aaa857a6828e12370a782cbe31
Codingwal <florian.herzog08@gmail.com>
2024-07-10 18:39:03 +0200

    Renamed directory :D

--- Assets/5_Terrain/Scripts/MapMeshGeneration/TreeMeshGenerator.cs
+++ Assets/5_Terrain/Scripts/MapMeshGeneration/TreeMeshGenerator.cs
@@ -12,14 +12,14 @@ public static class TreeMeshGenerator
         // Create a list for each material (number of treeTypes * materials per treeType (trunk & leaf))
         List<CombineInstance>[] meshes = new List<CombineInstance>[treeMeshes.treeMeshes.Count * 2];
 
-        // Intialize all lists
+        // Intialize the lists
         for (int i = 0; i < meshes.Length; i++)
         {
             meshes[i] = new();
         }
 
 
-        // Foreach tree...
+        // For each tree...
         for (int i = 0; i < map.trees.Count; i++)
         {
             TreeData tree = map.trees[i];
@@ -47,15 +47,9 @@ public static class TreeMeshGenerator
             };
 
             // Index = treeTypeIndex * indicesPerTreeType [1 for trunk, 1 for leaf] + 1 if its the leafMesh
-            Debug.Assert(meshes[(int)tree.type * 2] != null, "1");
-            // Debug.Assert(trunkCombineInstance != null, "2");
             meshes[(int)tree.type * 2].Add(trunkCombineInstance);
             meshes[(int)tree.type * 2 + 1].Add(leafCombineInstance);
         }
-
-        // Generate a default Matrix which is needed for the CombineInstances of the trunkMesh & leafMesh
-        Matrix4x4 defaultTransform = Matrix4x4.identity;
-        defaultTransform.SetTRS(new(0, 0, 0), Quaternion.identity, new(1, 1, 1));
 
         // Combine all meshes into one mesh for each material
         CombineInstance[] combineInstances = new CombineInstance[meshes.Length];
@@ -64,12 +58,11 @@ public static class TreeMeshGenerator
             Mesh tmpMesh = new() { indexFormat = UnityEngine.Rendering.IndexFormat.UInt32 };
             tmpMesh.CombineMeshes(meshes[i].ToArray());
             combineInstances[i].mesh = tmpMesh;
-            combineInstances[i].transform = defaultTransform;
         }
 
         // Combine the different meshes (one for each material) into one mesh but keep them seperated using submeshes
         Mesh mesh = new() { indexFormat = UnityEngine.Rendering.IndexFormat.UInt32 };
-        mesh.CombineMeshes(combineInstances, false);
+        mesh.CombineMeshes(combineInstances, false, false);
 
         return mesh;
     }
